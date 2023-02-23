70f8cb478c910100c31eb7cc01664230696985a7
Ali <2085836@cognizant.com>
2023-02-23 18:05:26 +0530

    initial commit

---
+++ Controllers/BankingController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bankingAppMVC.Controllers
+{
+    [Authorize]
+    public class BankingController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult BankingInfo()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult ContactBanking()
+        {
+            return View();
+        }
+
+        public IActionResult TransferFunds()
+        {
+            return View();
+        }
+
+        public IActionResult DownloadStatements()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
